be9279efcd9e0eae3cb8dd1b0002e08cf1aa4619
shahharshil46 <shahharshil46@gmail.com>
2025-06-05 00:22:27 -0700

    CFN stack for simple website - successful deployment

--- lib/static_website_demo-stack.ts
+++ lib/static_website_demo-stack.ts
@@ -1,16 +1,98 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class StaticWebsiteDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // S3 bucket
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'StaticWebsiteDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Origin Access Control
+    const oac = new cloudfront.CfnOriginAccessControl(this, 'S3OAC', {
+      originAccessControlConfig: {
+        name: 'S3-OAC',
+        description: 'OAC for CloudFront to access S3',
+        originAccessControlOriginType: 's3',
+        signingBehavior: 'always',
+        signingProtocol: 'sigv4',
+      },
+    });
+
+    // CloudFront Distribution
+    const distribution = new cloudfront.Distribution(this, 'WebsiteDistribution', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: 'index.html',
+    });
+
+    // Grant CloudFront access to S3
+    websiteBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [`${websiteBucket.bucketArn}/*`],
+      principals: [new iam.ServicePrincipal('cloudfront.amazonaws.com')],
+      conditions: {
+        StringEquals: {
+          'AWS:SourceArn': `arn:aws:cloudfront::${this.account}:distribution/${distribution.distributionId}`,
+        },
+      },
+    }));
+
+    // Deploy website to S3
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset('./website')],
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
+
+    new cdk.CfnOutput(this, 'WebsiteURL', {
+      value: `https://${distribution.domainName}`,
+    });
+
+    // DynamoDB Table
+    const table = new dynamodb.Table(this, 'StudentTable', {
+      partitionKey: { name: 'email', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // Lambda function
+    const registerFn = new lambda.Function(this, 'RegisterStudentFunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'register-student.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda')),
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    table.grantWriteData(registerFn);
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, 'StudentApi', {
+      restApiName: 'Student Service',
+    });
+
+    const registerResource = api.root.addResource('register');
+    registerResource.addMethod('POST', new apigateway.LambdaIntegration(registerFn));
+
+    new cdk.CfnOutput(this, 'APIEndpoint', {
+      value: `${api.url}register`,
+    });
   }
 }
